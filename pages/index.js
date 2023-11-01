9553de8e6466ce447adadbf849346266489f03c6
AmtTawsik <amttawsik.cse@gmail.com>
2023-11-01 21:45:51 +0600

    [Done] Edit Cursor

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
   const [selectedImages, setSelectedImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     setImages(imagesData);
     setLoading(false)
@@ -76,7 +75,7 @@ export default function Home() {
             {images?.map((image, index) => (
               <div
                 // Add conditional class for making 1st image big 
-                className={`w-full relative border rounded-lg hover:brightness-75 hover:bg-gray-300 ${index === 0 ? 'col-span-2 row-span-2' : 'col-span-1'}  ${selectedImages.includes(image.id) ? 'brightness-[90%] bg-gray-300' : ''}`}
+                className={`w-full cursor-move relative border rounded-lg hover:brightness-75 hover:bg-gray-300 ${index === 0 ? 'col-span-2 row-span-2' : 'col-span-1'}  ${selectedImages.includes(image.id) ? 'brightness-[90%] bg-gray-300' : ''}`}
                 key={image.id}
                 onClick={() => toggleSelectImage(image.id)} // Toggle checkbox when image is clicked
                 draggable // make all images draggable
@@ -88,14 +87,14 @@ export default function Home() {
                 <input
                   type="checkbox"
                   checked={selectedImages.includes(image.id)} // checkbox input
-                  className="absolute top-2 left-2"
+                  className="absolute top-2 left-2 cursor-pointer"
                 />
                 <img draggable="false" className="w-full rounded-lg" src={image.imgLink} alt={image.id} />
               </div>
             ))}
             <div className='col-span-2 md:col-span-1'>
               <div className="flex items-center justify-center w-full">
-                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100">
                   <div className="flex flex-col items-center justify-center pt-5 pb-6">
                     <svg className="w-8 h-8 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
